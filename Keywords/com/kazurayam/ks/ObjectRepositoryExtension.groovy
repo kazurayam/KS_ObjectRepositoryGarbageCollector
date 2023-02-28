70dfb414bd8e4726aac5cb9e74e693724572a7e9
kazurayam <kazuaki.urayama@gmail.com>
2023-02-28 10:16:49 +0900

    add Keywords/com/kazurayam/ks/ObjectRepositoryVisitor.groovy; add junit test example

---
+++ Keywords/com/kazurayam/ks/ObjectRepositoryExtension.groovy
@@ -0,0 +1,12 @@
+package com.kazurayam.ks
+
+/**
+ * This class extends the `com.kms.katalon.core.testobject.ObjectRepository` class and
+ * add some useful methods on the fly by Groovy's Meta-programming technique.
+ * 
+ * @author kazuryam
+ */
+public class ObjectRepositoryExtension {
+}
+
+
---
+++ Keywords/com/kazurayam/ks/ObjectRepositoryVisitor.groovy
@@ -0,0 +1,47 @@
+package com.kazurayam.ks
+
+import java.nio.file.FileVisitResult
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.SimpleFileVisitor
+import java.nio.file.attribute.BasicFileAttributes
+
+
+/**
+ * visits the dir to find out files of which name ends with ".rs".
+ * can return the list of rs files found. The returned list will
+ * contain relative Path to the dir given. 
+ * E.g,
+ *     "Page_CURA Healthcare Service/a_Go to Homepage.rs"
+ * will not be something like
+ *     "/Users/kazurayam/tmp/myproj/Object Repository/Page_CURA Healthcare Service/a_Go to Homepage.rs" 
+ */
+class ObjectRepositoryVisitor extends SimpleFileVisitor<Path> {
+
+	private Path dir
+	private List<Path> rsFiles
+
+	ObjectRepositoryVisitor(Path dir) {
+		Objects.requireNonNull(dir)
+		assert Files.exists(dir)
+		this.dir = dir
+		rsFiles = new ArrayList<Path>()
+	}
+
+	@Override
+	public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+		if (!Files.isDirectory(file) && file.getFileName().toString().endsWith(".rs")) {
+			Path relative = dir.relativize(file).toAbsolutePath().normalize()
+			rsFiles.add(relative);
+		}
+		return FileVisitResult.CONTINUE;
+	}
+
+	Path getDir() {
+		return dir
+	}
+
+	List<Path> getRsFiles() {
+		return rsFiles
+	}
+}
